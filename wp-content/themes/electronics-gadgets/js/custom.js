cd2e409dcbb7d6667837a15549af3c62e4a58f8f
mavenbiztechnews <mavenbiztechnews@gmail.com>
2022-11-19 16:09:18 +0530

    adeed

--- wp-content/themes/electronics-gadgets/js/custom.js
+++
@@ -1,8 +0,0 @@
-(function( $ ) {
-	jQuery(document).ready(function(){
-		jQuery(".product-cat").hide();
-	  jQuery("button.product-btn").click(function(){
-	    jQuery(".product-cat").toggle();
-	  });
-	});
-})( jQuery );
